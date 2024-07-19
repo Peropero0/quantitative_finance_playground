8c202c3bdb8089778b8efe0963afa1dd6fbd2d60
piripiri0 <lu.battistoni@gmail.com>
2024-07-19 17:40:00 +0200

    just keep the representation files

--- notebooks/Hasbrouck_Market_Microstructure/order_book_simulations/trade.py
+++
@@ -1,7 +0,0 @@
-
-class Trade:
-    def __init__(self, price, volume, direction):
-        self.price = price
-        self.volume = volume
-        self.direction = direction
-        # you can add trade id and trader id
