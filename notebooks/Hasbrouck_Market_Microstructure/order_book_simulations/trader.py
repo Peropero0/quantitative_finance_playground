a0e48a46bacbbe7bef65a96ae2a5b98b5b830916
piripiri0 <lu.battistoni@gmail.com>
2024-07-20 23:51:34 +0200

    order flow simulator

---
+++ notebooks/Hasbrouck_Market_Microstructure/order_book_simulations/trader.py
@@ -0,0 +1,17 @@
+from order import Order
+from order_book import OrderBook
+
+class Trader():
+    def __init__(self):
+        # here you can set different trader attributes
+        # like the initial cash, the trading strategy type, the risk aversion, etc...
+        #self.cash = initial_cash
+        #self.trader_id = trader_id
+        pass
+
+    def submit_order_to_order_book(self, order_type, price, quantity, book: OrderBook):
+        order = Order(order_type=order_type, price=price, quantity=quantity)
+
+        order.print_order()
+
+        book.add_order_to_the_order_book(order)
